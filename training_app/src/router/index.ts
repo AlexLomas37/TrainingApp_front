b9cee4faad77350b05a52675b50bb33ade53df1b
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-01 14:05:51 -0400

    feat: Creation du projet front avec vue.js

--- training_app/src/router/index.ts
+++ training_app/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DisciplinesView from '../views/Disciplines/DisciplinesView.vue'
+import DisciplineView from '../views/Disciplines/DisciplineView.vue'
+import TrainingView from '../views/TrainingView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +13,21 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/disciplines',
+      name: 'disciplines',
+      component: DisciplinesView,
     },
+    {
+      path: '/discipline/:id',
+      name: 'discipline',
+      component: DisciplineView,
+      props: true
+    },
+    {
+      path: '/training/:id',
+      name: 'disciplineTrainings',
+      component: TrainingView
+    }
   ],
 })
 
--- training_app/vite.config.ts
+++ training_app/vite.config.ts
@@ -8,12 +8,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000,
     open: true,
     cors: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://localhost:8090',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
