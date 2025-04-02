9c4f62906b8c05eac55d0de7ace9bd944e2b2650
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-02 11:32:57 -0400

    feat: Ajout de la fonctionnalité ajouter un exercice

--- training_app/src/components/AddExerciceItem.vue
+++ training_app/src/components/AddExerciceItem.vue
@@ -12,14 +12,25 @@
             </div>
             <div>
                 <label for="exTime">Temps (en minutes) :</label>
-                <input id="exTime" type="number" v-model="exercice.time" required />
+                <input id="exTime" type="number" v-model="exercice.time" />
             </div>
-            <button type="submit">Ajouter l'exercice</button>
+            <div>
+                <label for="exRep">Nombre de répétitions :</label>
+                <input id="exRep" type="number" v-model="exercice.repetition" />
+            </div>
+            <div class="button-container">
+                <GenericButton type="button" color="red" desktopText="Fermer" mobileText="Fermer" @click="closePopup" />
+                <GenericButton type="submit" desktopText="Ajouter l'exercice" mobileText="Ajouter" />
+            </div>
         </form>
     </div>
 </template>
 
 <script>
+import GenericButton from '@/components/utils/GenericButton.vue';
+
+const BASE_API_URL = import.meta.env.DEV ? '/api' : `${import.meta.env.VITE_API_URL}`;
+
 export default {
     name: 'AddExerciceItem',
     props: {
@@ -28,20 +39,52 @@ export default {
             required: true
         }
     },
+    components: {
+        GenericButton
+    },
     data() {
         return {
             exercice: {
                 name: '',
                 description: '',
-                time: null
+                time: null,
+                repetitions: null
             }
         }
     },
     methods: {
         submitExercice() {
-            const exerciceData = { ...this.exercice, trainingId: this.trainingId };
-            console.log("Exercice soumis :", exerciceData);
-            // TODO: ajouter la validation et le traitement de la soumission
+            const exerciceData = { ...this.exercice, training: { id: this.trainingId } };
+            if (!exerciceData.time || exerciceData.time <= 0) {
+                exerciceData.time = 0;
+            }
+            if (!exerciceData.repetition || exerciceData.repetition <= 0) {
+                exerciceData.repetition = 1;
+            }
+            console.log(exerciceData);
+            fetch(`${BASE_API_URL}/exercices`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(exerciceData)
+            })
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Erreur lors de l\'ajout de l\'exercice');
+                    }
+                    return response.json();
+                })
+                .then(data => {
+                    this.$emit('close');
+                    window.location.reload();
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        },
+        closePopup() {
+            this.$emit('close');
         }
     }
 }
@@ -51,5 +94,10 @@ export default {
 .card {
     padding: 1rem;
     margin: 1rem;
+}
+.button-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
