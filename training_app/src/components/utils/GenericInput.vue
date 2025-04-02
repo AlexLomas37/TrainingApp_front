95a927a0280a21290f9af4570d0441449ce69614
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-02 11:50:41 -0400

    feat: Ajout d'un component pour gérer les input

---
+++ training_app/src/components/utils/GenericInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps<{
+  modelValue: string | number;
+  type?: string;
+  placeholder?: string;
+}>();
+
+const emit = defineEmits(['update:modelValue']);
+
+function onInput(e: Event) {
+  const target = e.target as HTMLInputElement;
+  emit('update:modelValue', target.value);
+}
+</script>
+
+<template>
+  <input
+    class="input"
+    :type="type || 'text'"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="onInput"
+  />
+</template>
+
+<style scoped>
+.input {
+  background-color: #3d3d3d;
+  border-radius: 12px;
+  border: 0;
+  box-sizing: border-box;
+  color: #eee;
+  font-size: 18px;
+  height: 50px;
+  outline: 0;
+  padding: 4px 20px;
+  transition: all 0.3s ease;
+  margin-bottom: 20px;
+}
+</style>
