b85f0cbfd5ba2d892921113be874e3f7d7e29609
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-11 18:04:41 -0400

    feat: Ajout de boutons pour pouvoir modifier les différents oobjets

---
+++ training_app/src/components/utils/EditButtons.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="display: flex; gap: 1rem;">
+        <GenericButton icon="save" color="#4caf50" type="button"
+            @click="onSave && onSave()" />
+        <GenericButton icon="cancel" color="rgb(46, 46, 46)" type="button"
+            @click="onCancel && onCancel()" />
+        <GenericButton icon="delete" color="red" type="button"
+            @click="onDelete && onDelete()" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import GenericButton from '@/components/utils/GenericButton.vue';
+
+const props = defineProps({
+    onSave: { type: Function, default: null },
+    onCancel: { type: Function, default: null },
+    onDelete: { type: Function, default: null }
+});
+</script>
