a99f453c03e3b0a24619de3dd54be4105af80c54
Alex_Lomas <alex.lomaszewicz@gmail.com>
2025-04-15 20:16:10 -0400

    feat: Ajout d'un bouton de redirection vers la liste des disciplines

--- training_app/src/views/HomeView.vue
+++ training_app/src/views/HomeView.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import GenericButton from '../components/utils/GenericButton.vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function handleShowDisciplines() {
+  router.push('/disciplines')
+}
 </script>
 
 <template>
   <main>
     <h1>La seule application de gestion de vos entraînements !</h1>
+    <GenericButton 
+      desktopText="Afficher les disciplines" 
+      mobileText="Disciplines" 
+      icon="list" 
+      @click="handleShowDisciplines" 
+    />
   </main>
 </template>
